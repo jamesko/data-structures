0b12822d5392a68081c49aae0876f88d2a62532e
James Kolonusz <jamesak@gmail.com>
2014-12-11 15:34:58 -0800

    Add code to allow sequential adding and removing.

--- sprint-one/src/functional-shared/stack.js
+++ sprint-one/src/functional-shared/stack.js
@@ -1,17 +1,24 @@
 var Stack = function(){
-  var obj = {count: 0};
+  var obj = {count   : 0, 
+             storage : {}
+  };
   _.extend(obj, stackMethods);
   return obj;
 };
 
 var stackMethods = {
-  push  :  function(){
+  push  :  function(value){
     this.count += 1;
+    this.storage[this.count] = value;
   },
 
   pop   :  function(){
+    var temp =  this.storage[this.count];
+    delete this.storage[this.count];
+
     if (this.count > 0) {
       this.count -= 1;
+      return temp;
     }
 
 
