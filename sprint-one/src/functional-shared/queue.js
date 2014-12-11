ac652f3eee2fe57e0784415eab4eda4c0a3917aa
James Kolonusz <jamesak@gmail.com>
2014-12-11 15:46:29 -0800

    Reports a size of 0 for a new queue.

--- sprint-one/src/functional-shared/queue.js
+++ sprint-one/src/functional-shared/queue.js
@@ -1,9 +1,22 @@
 var Queue = function(){
-  // Hey! Rewrite in the new style. Your code will wind up looking very similar,
-  // but try not not reference your old code in writing the new style.
+    var obj = {};
+    
+    _.extend(obj, queueMethods);
+    
+    return obj;
 };
 
-var queueMethods = {};
+var queueMethods = {
+    
+    enqueue: function(){},
+    
+    dequeue: function(){},
+    
+    size   : function(){
+        return 0;
+    }
+    
+};
 
 
 
