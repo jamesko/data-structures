f1037f1fe81dff76fb9296d8463ec7849119b1fd
James Kolonusz <jamesak@gmail.com>
2014-12-11 00:10:20 -0800

    Report size of 0 for new stack.

--- sprint-one/src/functional-shared/stack.js
+++ sprint-one/src/functional-shared/stack.js
@@ -1,8 +1,13 @@
-var Stack = function() {
-  // Hey! Rewrite in the new style. Your code will wind up looking very similar,
-  // but try not not reference your old code in writing the new style.
+var Stack = function(){
+  var obj = {};
+  _.extend(obj, stackMethods);
+  return obj;
 };
 
-var stackMethods = {};
+var stackMethods = {
+  push  :  function(){},
+  pop   :  function(){},
+  size  :  function(){ return 0;}
+};
 
 
