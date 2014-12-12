b19b0e58f6429448598a92d4b9346de762626fdd
James Kolonusz <jamesak@gmail.com>
2014-12-11 21:48:02 -0800

    Fix report size 2 after adding two items.

--- sprint-one/src/pseudoclassical/stack.js
+++ sprint-one/src/pseudoclassical/stack.js
@@ -1,7 +1,15 @@
 var Stack = function() {
-
+  this.count = 0;
 };
 
-Stack.prototype.size = function() {return 0};
+Stack.prototype = {
+  push : function() {
+    this.count++;
+  },
+  pop  : function() {
+    this.count--;
+  },
+  size : function() {return this.count}
+};
 
 
