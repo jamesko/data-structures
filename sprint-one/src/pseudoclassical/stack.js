6def0c1fb2434ffc9a93df243282f585445658fe
James Kolonusz <jamesak@gmail.com>
2014-12-11 22:54:12 -0800

    Fix constructor prototype error.

--- sprint-one/src/pseudoclassical/stack.js
+++ sprint-one/src/pseudoclassical/stack.js
@@ -3,20 +3,20 @@ var Stack = function() {
   this.storage = {};
 };
 
-Stack.prototype = {
-  push : function(value) {
+Stack.prototype.push = function(value) {
     this.count++;
     this.storage[this.count] = value;
-  },
-  pop  : function() {
+  };
+  
+Stack.prototype.pop = function() {
     var temp = this.storage[this.count];
     delete this.storage[this.count];
     if (this.count > 0) {
       this.count--;
     }
     return temp;
-  },
-  size : function() {return this.count}
+  };
+  
+Stack.prototype.size = function() {
+    return this.count;
 };
-
-
