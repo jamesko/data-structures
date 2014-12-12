ba7325e37cbe83cd4d1037fe142cd3035d1edf71
James Kolonusz <jamesak@gmail.com>
2014-12-11 22:17:37 -0800

    Fix report 0, no err removing from empty queue and recommit.

--- sprint-one/src/pseudoclassical/queue.js
+++ sprint-one/src/pseudoclassical/queue.js
@@ -1,6 +1,22 @@
 var Queue = function() {
+    this.count = 0;
     
 };
 
-Queue.prototype.size = function(){return 0};
+Queue.prototype = {
+    
+    enqueue : function(){
+        this.count++;
+    },
+    
+    dequeue : function(){
+        if(this.count > 0){
+        this.count--;
+        }
+    },
+    
+    size : function(){
+        return this.count;
+    }
+}
 
--- sprint-one/src/pseudoclassical/stack.js
+++ sprint-one/src/pseudoclassical/stack.js
@@ -1,15 +1,20 @@
 var Stack = function() {
   this.count = 0;
+  this.storage = {};
 };
 
 Stack.prototype = {
-  push : function() {
+  push : function(value) {
     this.count++;
+    this.storage[this.count] = value;
   },
   pop  : function() {
+    var temp = this.storage[this.count];
+    delete this.storage[this.count];
     if (this.count > 0) {
       this.count--;
     }
+    return temp;
   },
   size : function() {return this.count}
 };
