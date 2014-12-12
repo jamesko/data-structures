6962025d1ac41d7758fb0a9cafe0559251dbc006
James Kolonusz <jamesak@gmail.com>
2014-12-11 16:39:07 -0800

    Fix allow sequential adding and removing to stack.

--- sprint-one/src/prototypal/stack.js
+++ sprint-one/src/prototypal/stack.js
@@ -1,17 +1,22 @@
 var Stack = function() {
   var obj = Object.create(stackMethods);
+  obj.storage = {};
   obj.count = 0;
   return obj;
 };
 
 var stackMethods = {
-  push : function() {
+  push : function(value) {
     this.count++;
+    this.storage[this.count] = value;
   },
   pop: function(){
+    var temp = this.storage[this.count];
+    delete this.storage[this.count];
     if (this.count > 0) {
       this.count --;
     }
+    return temp;
   },
   size : function(){
     return this.count;
