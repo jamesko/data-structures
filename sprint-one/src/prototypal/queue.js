ededb4aea2369d0e4be07b763b0a480d16039679
James Kolonusz <jamesak@gmail.com>
2014-12-11 21:10:47 -0800

    Fix allow sequential adding and removing of items.

--- sprint-one/src/prototypal/queue.js
+++ sprint-one/src/prototypal/queue.js
@@ -1,20 +1,29 @@
 var Queue = function() {
     var obj = Object.create(queueMethods);
     obj.count = 0;
+    obj.storage = {};
     return obj;
 };
 
 var queueMethods = {
     
-    enqueue: function(){
+    enqueue: function(value){
         this.count++;
+        this.storage[this.count] = value;
     },
     
     dequeue: function(){
+        var temp = this.storage[1];
+        var buffer = {};
+        delete this.storage[1];
+        for (var key in this.storage) {
+            buffer[key-1] = this.storage[key];
+        }
+        this.storage = buffer;
         if(this.count > 0) {
             this.count--;
         }
-        
+        return temp;
     },
     
     size: function(){
