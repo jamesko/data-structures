69a57b0ee529fe58fa01ea107025f77b6b746068
James Kolonusz <jamesak@gmail.com>
2014-12-11 22:48:43 -0800

    Fix prototype constructor error.

--- sprint-one/src/pseudoclassical/queue.js
+++ sprint-one/src/pseudoclassical/queue.js
@@ -1,22 +1,29 @@
 var Queue = function() {
-    this.count = 0;
-    
+  this.count = 0;
+  this.storage = {};
 };
 
-Queue.prototype = {
+Queue.prototype.enqueue = function(value){
+  this.count++;
+  this.storage[this.count] = value;
+};
     
-    enqueue : function(){
-        this.count++;
-    },
+Queue.prototype.dequeue = function(){
+  var temp = this.storage[1];
+  delete this.storage[1];
+  var buffer = {};
+  for(var key in this.storage){
+    buffer[key-1] = this.storage[key];
+  }
+  this.storage = buffer;
+
+  if(this.count > 0){
+    this.count--;
+  }
+  return temp;
+};
     
-    dequeue : function(){
-        if(this.count > 0){
-        this.count--;
-        }
-    },
-    
-    size : function(){
-        return this.count;
-    }
-}
+Queue.prototype.size = function(){
+  return this.count;
+};
 
